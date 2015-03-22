7c49a4d4e595a8d9ebd7519711e6c9d4261add66
Jayanth Ram K.V <jayanthram1991@gmail.com>
2015-03-22 21:24:17 +0530

    removed un-wanted code

--- application/controllers/Users.php
+++ application/controllers/Users.php
@@ -83,51 +83,14 @@ class Users extends CI_Controller {
 
 			printf("seeding %d of %d user(s)<br>\n",$i+1,$limit);
 
-			$this->user_id 		= $this->generate_user_id();
 			$this->email_id 	= $this->faker->email;
 			$this->created_date = time();
 			$this->updated_date = $this->created_date;
 			$mt_rand_boolean	= mt_rand(0, 1);
 			$this->site_id  	= mt_rand(0, 1) ? "151336557" : "151323689";
 
-			$data = array(
-			              'user_id'         => $this->user_id,
-			              'password'        => md5("123456"),
-			              'email_id'        => $this->email_id,
-			              'status'          => mt_rand(1, 2),
-			              'created_date'    => $this->created_date,
-			              'updated_date'    => $this->updated_date,
-			              );
-
-			// create user
-			$this->db->insert($this->table_users,$data);
-
 			// create profile
 			$profile_id = $this->_create_profile();
-
-			// create user profile mapping
-			// $this->_create_user_profile($user_id, $profile_id);
-
-			// create profile email
-			// $this->_create_profile_email($user_id, $profile_id, $email_id);
-
-			// create member
-			// $this->_create_member($user_id, $site_id);
-
-			// email verifications (optional)
-			// $this->_create_email_verify($user_id, $email_id);
-
-			// create profile address
-			// $this->_create_profile_address($user_id, $profile_id, $site_id);
-
-			// create profile contact
-			// $this->_create_profile_contact($user_id, $profile_id, $site_id);
-
-			// create profile education
-			// $this->_create_profile_education($user_id, $profile_id, $site_id);
-
-			// create profile work
-			// $this->_create_profile_work($user_id, $profile_id, $site_id);
 		}
 
 		$this->benchmark->mark('code_end');
@@ -146,7 +109,6 @@ class Users extends CI_Controller {
 		$mt_rand = mt_rand(0, 1);
 
 		$p = array(
-		           'user_id'        => $this->user_id,
 		           'profile'		=> $this->get_profile(),
 		           'address'		=> $this->get_address(),
 		           'contact'		=> $this->get_contact(),
@@ -159,285 +121,13 @@ class Users extends CI_Controller {
 
 		$this->db->insert($this->table_profiles, $p);
 
+		$p["user_id"] = $this->db->insert_id();
+
 		// index elasticsearch
 		$this->index_elasticsearch($p);
 
 		return $this->db->insert_id();
 	}
-
-	public function get_profile($json=TRUE)
-	{
-		$mt_rand = mt_rand(0, 1);
-		$profile = array(
-		                 'email_id' 	=> $this->email_id,
-		                 'first_name'   => $this->faker->firstName,
-		                 'middle_name'  => "",
-		                 'last_name'    => $this->faker->lastName,
-		                 'salutation'   => $this->faker->title,
-		                 'birthday' 	=> $this->faker->dateTimeThisCentury->format('Y-m-d'),
-		                 'gender' 		=> $mt_rand ? "M" : "F",
-		                 'picture' 		=> $this->faker->imageURL(),
-		                 'relationship' => mt_rand(0, 3),
-		                 'headline' 	=> $this->faker->realText(150),
-		                 'about_me' 	=> $this->faker->realText(150),
-		                 'score' 		=> (5 % mt_rand(5, 10)) ? 5 * mt_rand(1, 26) : 10 * mt_rand(1, 13),
-		                 );
-
-		return ($json) ? json_encode($profile) : $profile;
-	}
-
-	public function get_address($json=TRUE)
-	{
-		$address =array();
-		$mt_rand = mt_rand(1, 3);
-		for ($i=0; $i < $mt_rand; $i++) {
-			$address[$i] = array(
-			                     'address' 		=> $this->faker->streetAddress,
-			                     'city' 		=> $this->faker->city,
-			                     'state' 		=> $this->faker->state,
-			                     'country' 		=> $this->faker->country,
-			                     'pincode' 		=> $this->faker->postcode,
-			                     'type'	=> mt_rand(1, 2),
-			                     'current'		=> mt_rand(0, 1),
-			                     );
-		}
-
-		return ($json) ? json_encode($address) : $address;
-	}
-
-	public function get_contact($json=TRUE)
-	{
-		$contact =array();
-		$mt_rand = mt_rand(1, 4);
-
-		for ($i=0; $i < $mt_rand; $i++) {
-			$contact[$i] = array(
-			                     'number'	=> $this->faker->phoneNumber,
-			                     'type'		=> $mt_rand,
-			                     );
-		}
-		return ($json) ? json_encode($contact) : $contact;
-	}
-
-	public function get_education($json=TRUE)
-	{
-		$education =array();
-		$mt_rand = mt_rand(0, 1);
-		$e_rand = mt_rand(1, 4);
-		$end_date = $this->faker->dateTimeThisCentury->format('Y');
-
-		for ($i=0; $i < $e_rand; $i++) {
-			$education[$i] = array(
-			                       'name' 			=> $this->faker->state."-".$this->in_name[array_rand($this->in_name)],
-			                       'type' 			=> $e_rand,
-			                       'course'			=> $this->course[array_rand($this->course)],
-			                       'degree'			=> $this->degree[array_rand($this->degree)],
-			                       'current'		=> $mt_rand,
-			                       'start_date'		=> $end_date - mt_rand(3, 4),
-			                       'end_date'		=> $end_date,
-			                       );
-		}
-
-		return ($json) ? json_encode($education) : $education;
-	}
-
-	public function get_work($json=TRUE)
-	{
-		$work = array();
-		$mt_rand 	= mt_rand(0, 1);
-		$w_rand 	= mt_rand(1, 4);
-		$end_year 	= $this->faker->dateTimeThisCentury->format('Y');
-		$end_month 	= $this->faker->dateTimeThisCentury->format('m');
-
-		for ($i=0; $i <$w_rand ; $i++) {
-			$work[$i] = array(
-			                  'organization'=> $this->faker->company,
-			                  'position'  	=> $this->faker->catchPhrase,
-			                  'function'  	=> $this->faker->bs,
-			                  'location'	=> $this->faker->city,
-			                  'start_month'	=> $end_month - $mt_rand,
-			                  'end_month'	=> $end_month,
-			                  'start_year'	=> $end_year- mt_rand(1,20),
-			                  'end_year'	=> $end_year,
-			                  'current'		=> $mt_rand,
-			                  );
-		}
-
-		return ($json) ? json_encode($work) : $work;
-	}
-
-	public function _create_user_profile($user_id='', $profile_id='')
-	{
-		$u_p = array(
-		             'user_id' 		=> $user_id,
-		             'profile_id' 	=> $profile_id,
-		             'status'		=> mt_rand(1, 3),
-		             );
-
-		$this->db->insert($this->table_user_profiles, $u_p);
-	}
-
-	public function _create_profile_email($user_id='', $profile_id='', $email_id='')
-	{
-		$mt_rand = mt_rand(0, 1);
-
-		$p_e =  array(
-		              'user_id' 	=> $user_id,
-		              'profile_id' 	=> $profile_id,
-		              'email_id' 	=> $email_id,
-		              'email_type'	=> mt_rand(1, 2),
-		              'status'		=> mt_rand(1, 2),
-		              'deleted'		=> $mt_rand,
-		              'suggestion'	=> $mt_rand,
-		              'show_flag'	=> $mt_rand,
-		              'by_admin'	=> $mt_rand,
-		              'created_date'=> $this->created_date,
-		              'created_by'  => $user_id,
-		              'updated_date'=> $this->updated_date,
-		              'updated_by'  => $user_id,
-		              );
-		$this->db->insert($this->table_profile_emails, $p_e);
-	}
-
-	public function _create_member($user_id='', $site_id='')
-	{
-		$m = array(
-		           'user_id' 		=> $user_id,
-		           'user_type'		=> mt_rand(1, 3),
-		           'label'			=> "Batch of ". $this->faker->dateTimeThisCentury->format('Y').",".$this->course[array_rand($this->course)],
-		           'step'			=> mt_rand(1, 4),
-		           'status'			=> mt_rand(-2, 3),
-		           'verified'		=> mt_rand(1, 2),
-		           'site_id'			=> $site_id,
-		           'deleted'			=> mt_rand(0,1),
-		           'approved_date'	=> $this->created_date,
-		           'created_date'	=> $this->created_date,
-		           'updated_date'	=> $this->updated_date,
-		           );
-		$this->db->insert($this->table_members, $m);
-	}
-
-	public function _create_profile_address($user_id='', $profile_id='', $site_id='')
-	{
-		$mt_rand = mt_rand(0, 1);
-		$p_a =  array(
-		              'user_id' 	=> $user_id,
-		              'profile_id' 	=> $profile_id,
-		              'address' 	=> $this->faker->streetAddress,
-		              'city' 		=> $this->faker->city,
-		              'state' 		=> $this->faker->state,
-		              'country' 	=> $this->faker->country,
-		              'pincode' 	=> $this->faker->postcode,
-		              'address_type'=> mt_rand(1, 2),
-		              'current'		=> $mt_rand,
-		              'site_id'		=> $site_id,
-		              'deleted'		=> $mt_rand,
-		              'suggestion'	=> $mt_rand,
-		              'show_flag'	=> $mt_rand,
-		              'by_admin'	=> $mt_rand,
-		              'created_date'=> $this->created_date,
-		              'created_by'  => $user_id,
-		              'updated_date'=> $this->updated_date,
-		              'updated_by'  => $user_id,
-		              );
-		$this->db->insert($this->table_profile_address, $p_a);
-	}
-
-	public function _create_profile_contact($user_id='', $profile_id='', $site_id='')
-	{
-		$mt_rand = mt_rand(0, 1);
-
-		$p_c = array(
-		             'user_id' 		=> $user_id,
-		             'profile_id' 		=> $profile_id,
-		             'contact_number' 	=> $this->faker->phoneNumber,
-		             'contact_type'	=> mt_rand(1, 4),
-		             'site_id'			=> $site_id,
-		             'deleted'			=> $mt_rand,
-		             'suggestion'		=> $mt_rand,
-		             'show_flag'		=> $mt_rand,
-		             'by_admin'		=> $mt_rand,
-		             'created_date'	=> $this->created_date,
-		             'created_by'  	=> $user_id,
-		             'updated_date'	=> $this->updated_date,
-		             'updated_by'  	=> $user_id,
-		             );
-		$this->db->insert($this->table_profile_contact, $p_c);
-	}
-
-	public function _create_profile_education($user_id='', $profile_id='', $site_id='')
-	{
-		$mt_rand = mt_rand(0, 1);
-		$end_date = $this->faker->dateTimeThisCentury->format('Y');
-
-		$p_e = array(
-		             'user_id' 		=> $user_id,
-		             'profile_id' 		=> $profile_id,
-		             'institute_name'  => $this->faker->state."-".$this->in_name[array_rand($this->in_name)],
-		             'institute_type'  => $mt_rand,
-		             'course'			=> $this->course[array_rand($this->course)],
-		             'degree'			=> $this->degree[array_rand($this->degree)],
-		             'current'			=> $mt_rand,
-		             'start_date'		=> $end_date - mt_rand(3, 4),
-		             'end_date'		=> $end_date,
-		             'site_id'			=> $site_id,
-		             'deleted'			=> $mt_rand,
-		             'suggestion'		=> $mt_rand,
-		             'show_flag'		=> $mt_rand,
-		             'by_admin'		=> $mt_rand,
-		             'created_date'	=> $this->created_date,
-		             'created_by'  	=> $user_id,
-		             'updated_date'	=> $this->updated_date,
-		             'updated_by'  	=> $user_id,
-		             );
-		$this->db->insert($this->table_profile_education, $p_e);
-	}
-
-	public function _create_profile_work($user_id='',$profile_id='',$site_id='')
-	{
-		$mt_rand = mt_rand(0, 1);
-		$end_year = $this->faker->dateTimeThisCentury->format('Y');
-		$end_month = $this->faker->dateTimeThisCentury->format('m');
-
-		$p_w = array(
-		             'user_id' 		=> $user_id,
-		             'profile_id' 		=> $profile_id,
-		             'organization'  	=> $this->faker->company,
-		             'position'  		=> $this->faker->catchPhrase,
-		             'function'  		=> $this->faker->bs,
-		             'location'		=> $this->faker->city,
-		             'start_month'		=> $end_month - $mt_rand,
-		             'end_month'		=> $end_month,
-		             'start_year'		=> $end_year- mt_rand(1,20),
-		             'end_year'		=> $end_year,
-		             'current'			=> $mt_rand,
-		             'site_id'			=> $site_id,
-		             'deleted'			=> $mt_rand,
-		             'suggestion'		=> $mt_rand,
-		             'show_flag'		=> $mt_rand,
-		             'by_admin'		=> $mt_rand,
-		             'created_date'	=> $this->created_date,
-		             'created_by'  	=> $user_id,
-		             'updated_date'	=> $this->updated_date,
-		             'updated_by'  	=> $user_id,
-		             );
-		$this->db->insert($this->table_profile_work, $p_w);
-	}
-
-	public function _create_email_verify($user_id='', $email_id='')
-	{
-		$e_v = array(
-		             'user_id' 		=> $user_id,
-		             'email_id'		=> $email_id,
-		             'hash'			=> random_string('alnum', 32),
-		             'type'			=> mt_rand(1, 5),
-		             'status'			=> mt_rand(1, 2),
-		             'created_date'	=> $this->created_date,
-		             );
-
-		$this->db->insert($this->table_email_verify, $e_v);
-	}
-
 
 	private function _truncate_db()
 	{
@@ -445,21 +135,6 @@ class Users extends CI_Controller {
 		$this->db->truncate($this->table_profiles);
 	}
 
-	function generate_user_id()
-	{
-		$user_id    =   random_string('numeric', 10) .  mt_rand(999, 99999);
-
-		// check if the user id exist in v4_users, if present generate new user id
-		$where["user_id"] = $user_id;
-
-		$is_record_exist = $this->db->get_where($this->table_users,$where);
-
-		if ($is_record_exist->num_rows() > 0) {
-			$this->generate_user_id();
-		} else {
-			return $user_id;
-		}
-	}
 	function convert($size)
 	{
 		$unit=array('b','kb','mb','gb','tb','pb');
